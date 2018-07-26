3b3df66dc990e1a985fac573e06b1e61c3e63940
rosskush <ross.kush@gmail.com>
2018-07-26 14:34:49 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='partipy',
+      version='0.1',
+      description='semi-analytical methods to track particles in groundwater',
+      author='Ross Kushnereit',
+      author_email='ross.kush@gmail.com',
+      url='intera.com',
+      packages=['partipy'],
+     )
