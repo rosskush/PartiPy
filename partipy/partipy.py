738a7cecdbee3407b21b1933445ec6819f53b46b
rosskush <ross.kush@gmail.com>
2018-07-26 14:45:05 -0500

    added examples from flopy docs

--- partipy/partipy.py
+++ partipy/partipy.py
@@ -1,17 +1,6 @@
-import os
-
-files = os.listdir()
-
-# print(files)
-gifs = [item for item in files if item.endswith('.gif')]
-
-print(gifs)
+import numpy as np
 
 
-for gif in gifs:
-	mp4 = gif.replace('.gif','.mp4')
-	if not os.path.exists(mp4):
-		print(gif)
-		arg = f'ffmpeg -i {gif} -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" {mp4}'
-		os.system(arg)
-		
+class track_particles():
+	def __init__(self):
+
